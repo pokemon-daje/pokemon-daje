89f01ed76f9c89bc45ffdad4e118849d997850d0
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-17 12:48:44 +0200

    feat (starting commit): base project structure

---
+++ src/main/java/com/pokemon/daje/DajeApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.daje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemon/daje/DajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.daje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
