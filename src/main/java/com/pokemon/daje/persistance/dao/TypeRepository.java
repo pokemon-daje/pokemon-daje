b9875e77e5fb0710ecbcaa3a5098d99c6f78b733
gsferruzzi <giuseppe.sferruzzi@alten.it>
2023-07-18 15:57:45 +0200

    refactor: move repository classes into persistence

--- src/main/java/com/pokemon/daje/persistance/dao/TypeRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/TypeRepository.java
@@ -1,4 +1,4 @@
-package com.pokemon.daje.persistance.dto.dao;
+package com.pokemon.daje.persistance.dao;
 
 import com.pokemon.daje.persistance.dto.TypeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
