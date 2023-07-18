7dd23aab914153c7a3cbc5627cc9ab25fa862da1
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-18 15:30:13 +0200

    refactor ( renamed Move, Pokemon, Type to be more informative as DTO )

--- src/main/java/com/pokemon/daje/persistance/dto/TypeDTO.java
+++ src/main/java/com/pokemon/daje/persistance/dto/TypeDTO.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "type")
-public class Type {
+public class TypeDTO {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int dbId;
