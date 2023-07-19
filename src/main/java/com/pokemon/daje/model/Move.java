bddca9bd616b20ae107cb6136d113556dad94539
mauronavi <mauro.naviglio@gmail.com>
2023-07-19 09:49:25 +0200

    Refactor(implements business interfaces)

--- src/main/java/com/pokemon/daje/model/Move.java
+++ src/main/java/com/pokemon/daje/model/Move.java
@@ -4,16 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 
-public class Move {
+public class Move implements BusinessInterface {
 
     private Integer id;
     private String name;
     private Type type;
     private Integer power;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Move move = (Move) o;
+        return id.equals(move.id) && name.equals(move.name) && type.equals(move.type) && power.equals(move.power);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type, power);
+    }
 }
 
