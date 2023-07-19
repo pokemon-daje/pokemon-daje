989b9d525289b53ea29f3c1ab0cc5325e93f4db0
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-19 16:51:51 +0200

    Feat(add getSixRandomPokemon method)

--- src/main/java/com/pokemon/daje/persistance/dao/PokemonRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/PokemonRepository.java
@@ -2,6 +2,10 @@ package com.pokemon.daje.persistance.dao;
 
 import com.pokemon.daje.persistance.dto.PokemonDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface PokemonRepository extends JpaRepository<PokemonDTO,Integer> {
+    @Query(value = "SELECT * FROM pokemons ORDER BY RAND() LIMIT 0,6;", nativeQuery = true)
+    List<PokemonDTO> getSixRandomPokemon();
 }
