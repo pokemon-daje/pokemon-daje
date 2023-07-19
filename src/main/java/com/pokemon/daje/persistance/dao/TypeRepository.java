319fdf9d33c6972efe702d95875a41fb6381db0e
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-19 13:01:25 +0200

    Refactor(add findByPokedexId method)

--- src/main/java/com/pokemon/daje/persistance/dao/TypeRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/TypeRepository.java
@@ -3,5 +3,8 @@ package com.pokemon.daje.persistance.dao;
 import com.pokemon.daje.persistance.dto.TypeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TypeRepository extends JpaRepository<TypeDTO,Integer> {
+    Optional<TypeDTO> findByPokedexId(int id);
 }
