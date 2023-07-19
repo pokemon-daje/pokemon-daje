71558eed1cb63758dc41052e7a9f9c1bc6e8fa0b
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-19 16:27:49 +0200

    Refactor(add assets images url)

--- scripts/insertType.sql
+++ scripts/insertType.sql
@@ -1,23 +1,22 @@
 INSERT INTO type(pokedex_type_id, image_url, name) VALUES
-    (1, '...', 'ice'),
-    (2, '...', 'ghost'),
-    (3, '...', 'fairy'),
-    (4, '...', 'dragon'),
-    (5, '...', 'electric'),
-    (6, '...', 'rock'),
-    (7, '...', 'steel'),
-    (8, '...', 'fighting'),
-    (9, '...', 'ground'),
-    (10, '...', 'poison'),
-    (11, '...', 'dark'),
-    (12, '...', 'fire'),
-    (13, '...', 'bug'),
-    (14, '...', 'psychic'),
-    (15, '...', 'flying'),
-    (16, '...', 'grass'),
-    (17, '...', 'normal'),
-    (18, '...', 'water');
-
+            (1, 'https://archives.bulbagarden.net/media/upload/1/15/Ice_icon_SwSh.png', 'ice'),
+            (2, 'https://archives.bulbagarden.net/media/upload/0/01/Ghost_icon_SwSh.png', 'ghost'),
+            (3, 'https://archives.bulbagarden.net/media/upload/c/c6/Fairy_icon_SwSh.png', 'fairy'),
+            (4, 'https://archives.bulbagarden.net/media/upload/7/70/Dragon_icon_SwSh.png', 'dragon'),
+            (5, 'https://archives.bulbagarden.net/media/upload/7/7b/Electric_icon_SwSh.png', 'electric'),
+            (6, 'https://archives.bulbagarden.net/media/upload/1/11/Rock_icon_SwSh.png', 'rock'),
+            (7, 'https://archives.bulbagarden.net/media/upload/0/09/Steel_icon_SwSh.png', 'steel'),
+            (8, 'https://archives.bulbagarden.net/media/upload/3/3b/Fighting_icon_SwSh.png', 'fighting'),
+            (9, 'https://archives.bulbagarden.net/media/upload/2/27/Ground_icon_SwSh.png', 'ground'),
+            (10, 'https://archives.bulbagarden.net/media/upload/8/8d/Poison_icon_SwSh.png', 'poison'),
+            (11, 'https://archives.bulbagarden.net/media/upload/d/d5/Dark_icon_SwSh.png', 'dark'),
+            (12, 'https://archives.bulbagarden.net/media/upload/a/ab/Fire_icon_SwSh.png', 'fire'),
+            (13, 'https://archives.bulbagarden.net/media/upload/9/9c/Bug_icon_SwSh.png', 'bug'),
+            (14, 'https://archives.bulbagarden.net/media/upload/7/73/Psychic_icon_SwSh.png', 'psychic'),
+            (15, 'https://archives.bulbagarden.net/media/upload/b/b5/Flying_icon_SwSh.png', 'flying'),
+            (16, 'https://archives.bulbagarden.net/media/upload/a/a8/Grass_icon_SwSh.png', 'grass'),
+            (17, 'https://archives.bulbagarden.net/media/upload/9/95/Normal_icon_SwSh.png', 'normal'),
+            (18, 'https://archives.bulbagarden.net/media/upload/8/80/Water_icon_SwSh.png', 'water');
 
 
 
