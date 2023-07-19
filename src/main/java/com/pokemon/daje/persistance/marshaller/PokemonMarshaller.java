379bfae774d6cd9866151023814944a893494616
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-19 13:00:52 +0200

    Refactor(add missing setters)

--- src/main/java/com/pokemon/daje/persistance/marshaller/PokemonMarshaller.java
+++ src/main/java/com/pokemon/daje/persistance/marshaller/PokemonMarshaller.java
@@ -14,11 +14,13 @@ import java.util.Set;
 public class PokemonMarshaller implements
         BaseMarshaller<Pokemon, PokemonDTO> {
 
-    MoveMarshaller moveMarshaller;
+    private final MoveMarshaller moveMarshaller;
+    private final TypeMarshaller typeMarshaller;
 
     @Autowired
-    private PokemonMarshaller(MoveMarshaller moveMarshaller){
+    private PokemonMarshaller(MoveMarshaller moveMarshaller, TypeMarshaller typeMarshaller){
         this.moveMarshaller = moveMarshaller;
+        this.typeMarshaller = typeMarshaller;
     }
 
     @Override
@@ -28,9 +30,14 @@ public class PokemonMarshaller implements
             pokemon = new Pokemon();
             pokemon.setId(dto.getId());
             pokemon.setName(dto.getName());
+            pokemon.setSpriteUrl(dto.getSpriteUrl());
+            pokemon.setCurrentHP(dto.getCurrentHealthPoints());
+            pokemon.setMaxHP(dto.getMaxHealthPoints());
+            pokemon.setType(typeMarshaller.fromDTO(dto.getType()));
             Set<Move> moveSet = new HashSet<>();
             dto.getMoveSet().forEach(move -> moveSet.add(moveMarshaller.fromDTO(move)));
             pokemon.setMoves(moveSet);
+            pokemon.setOriginalTrainer(dto.getTrainerName());
         }
         return pokemon;
     }
@@ -42,9 +49,14 @@ public class PokemonMarshaller implements
             pokemonDTO = new PokemonDTO();
             pokemonDTO.setId(business.getId());
             pokemonDTO.setName(business.getName());
+            pokemonDTO.setSpriteUrl(business.getSpriteUrl());
+            pokemonDTO.setCurrentHealthPoints(business.getCurrentHP());
+            pokemonDTO.setMaxHealthPoints(business.getMaxHP());
+            pokemonDTO.setType(typeMarshaller.toDTO(business.getType()));
             Set<MoveDTO> moveSet = new HashSet<>();
             business.getMoves().forEach(move -> moveSet.add(moveMarshaller.toDTO(move)));
             pokemonDTO.setMoveSet(moveSet);
+            pokemonDTO.setTrainerName(business.getOriginalTrainer());
         }
         return pokemonDTO;
     }
