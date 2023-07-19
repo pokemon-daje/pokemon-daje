d82040c93c17f15238bac8a2f468541e29e36ed4
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-19 12:50:52 +0200

    Feat(add pokemon/move types to DB)

---
+++ scripts/insertType.sql
@@ -0,0 +1,23 @@
+INSERT INTO type(pokedex_type_id, image_url, name) VALUES
+    (1, '...', 'ice'),
+    (2, '...', 'ghost'),
+    (3, '...', 'fairy'),
+    (4, '...', 'dragon'),
+    (5, '...', 'electric'),
+    (6, '...', 'rock'),
+    (7, '...', 'steel'),
+    (8, '...', 'fighting'),
+    (9, '...', 'ground'),
+    (10, '...', 'poison'),
+    (11, '...', 'dark'),
+    (12, '...', 'fire'),
+    (13, '...', 'bug'),
+    (14, '...', 'psychic'),
+    (15, '...', 'flying'),
+    (16, '...', 'grass'),
+    (17, '...', 'normal'),
+    (18, '...', 'water');
+
+
+
+
