9317e99c5bd1c6549f65becf511d4f6ad6c389df
mmazzilli <marika.mazzilli@alten.it>
2023-07-20 17:58:09 +0200

    add (modal on card): add modal on card and add bootstrap.

--- daje-fe/fetch.js
+++ daje-fe/fetch.js
@@ -18,6 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
                        <div class="img"><img src="${pokemon_spacies.sprite_url}" draggable="false"></div>
                        <h2>${pokemon_spacies.name}</h2>
                        <span>Web Developer</span> 
+
+                       <button type="button" class="btn" id="btn_modal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        me voi vedè bene?
+                       </button>
+
+                       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                           <div class="modal-header">
+                              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                           </div>
+                           <div class="modal-body">
+                              ...
+                           </div>
+                           <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Save changes</button>
+                           </div>
+                        </div>
+                        </div>
+                     </div>
                    </li>  
                    `
                }
