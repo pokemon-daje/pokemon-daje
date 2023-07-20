70a3dbe7f17faa5a68b2bc6270a5198d12d9d187
AlessandroTrupia <trupia.alessandro@libero.it>
2023-07-20 13:02:28 +0200

    Feature (PokemonSpeciesRepository): add findByPokedexId feature

--- src/main/java/com/pokemon/daje/persistance/dao/PokemonSpeciesRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/PokemonSpeciesRepository.java
@@ -3,5 +3,9 @@ package com.pokemon.daje.persistance.dao;
 import com.pokemon.daje.persistance.dto.PokemonSpeciesDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PokemonSpeciesRepository extends JpaRepository<PokemonSpeciesDTO, Integer> {
+
+    Optional<PokemonSpeciesDTO> findByPokedexId(Integer integer);
 }
