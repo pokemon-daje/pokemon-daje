59fc8731375f287e95bebe64bbdb1adc4253ac34
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-20 14:24:36 +0200

    feat ( add findByPokedexId )

--- src/main/java/com/pokemon/daje/persistance/dao/MoveRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/MoveRepository.java
@@ -3,5 +3,8 @@ package com.pokemon.daje.persistance.dao;
 import com.pokemon.daje.persistance.dto.MoveDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MoveRepository extends JpaRepository<MoveDTO,Integer> {
+    Optional<MoveDTO> findByPokedexId(int id);
 }
