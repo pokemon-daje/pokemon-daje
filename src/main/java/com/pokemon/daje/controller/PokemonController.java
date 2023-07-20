6ae640434ae92b3503588a01405467e74987702f
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-20 23:23:21 +0200

    feat ( add server sent events to notify FE of an exchange )

--- src/main/java/com/pokemon/daje/controller/PokemonController.java
+++ src/main/java/com/pokemon/daje/controller/PokemonController.java
@@ -2,20 +2,30 @@ package com.pokemon.daje.controller;
 
 import com.pokemon.daje.model.Pokemon;
 import com.pokemon.daje.service.PokemonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class PokemonController {
-
     final PokemonService pokemonService;
+    private Set<SseEmitter> serverEmitters;
 
     @Autowired
     public PokemonController(PokemonService pokemonService) {
         this.pokemonService = pokemonService;
+        serverEmitters =new HashSet<>();
     }
 
     @GetMapping("/pokemons")
@@ -35,7 +45,42 @@ public class PokemonController {
 
     @PostMapping("/pokemons/swap")
     public Pokemon swap(@RequestBody Pokemon pokemon) {
-        return pokemonService.swap(pokemon);
+        Pokemon toSend = pokemonService.swap(pokemon);
+        List<SseEmitter> usedEmitter = new ArrayList<>();
+        serverEmitters.forEach(sseEmitter -> {
+                    try {
+                        sseEmitter.send(SseEmitter.event()
+                                .data(pokemonService.getListOfChagedPokemon())
+                                .id("exchange")
+                                .name("pokemon"));
+                        usedEmitter.add(sseEmitter);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+        usedEmitter.forEach(ResponseBodyEmitter::complete);
+        return toSend;
+    }
+
+    @GetMapping("/pokemons/exchange")
+    public SseEmitter streamSseMvc() {
+        SseEmitter emitter = new SseEmitter(2000L);
+            try {
+                SseEmitter.SseEventBuilder event = SseEmitter.event()
+                        .data(pokemonService.getListOfChagedPokemon())
+                        .id("exchange")
+                        .name("pokemon");
+                emitter.send(event);
+
+                serverEmitters.add(emitter);
+                emitter.onCompletion(()->{serverEmitters.remove(emitter);});
+                emitter.onTimeout(emitter::complete);
+
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        return emitter;
     }
 
     @DeleteMapping("/pokemons/{id}")
