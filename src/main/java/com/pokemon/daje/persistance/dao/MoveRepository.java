a82d4c7668205b9228295ba520f24c619831403c
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-24 18:53:23 +0200

    feat ( add method to find move by pokedexId or else fallback to unknown specie )

--- src/main/java/com/pokemon/daje/persistance/dao/MoveRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/MoveRepository.java
@@ -2,9 +2,13 @@ package com.pokemon.daje.persistance.dao;
 
 import com.pokemon.daje.persistance.dto.MoveDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MoveRepository extends JpaRepository<MoveDTO,Integer> {
     Optional<MoveDTO> findByPokedexId(int id);
+    @Query("select m from MoveDTO as m where m.pokedexId = :id or m.pokedexId = 30000 ORDER BY(m.pokedexId) limit 1")
+    Optional<MoveDTO> findByPokedexIdOrGetUnknow(@Param("id") int id);
 }
