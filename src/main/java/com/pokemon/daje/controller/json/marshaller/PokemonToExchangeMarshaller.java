df1a913c6492c0a70ccfcc4e2417ee4fa147ad71
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-24 19:00:02 +0200

    feat ( add marshaller for exchange pokemons )

---
+++ src/main/java/com/pokemon/daje/controller/json/marshaller/PokemonToExchangeMarshaller.java
@@ -0,0 +1,43 @@
+package com.pokemon.daje.controller.json.marshaller;
+
+import com.pokemon.daje.controller.json.dto.PokemonExchangeDTO;
+import com.pokemon.daje.model.Move;
+import com.pokemon.daje.model.Pokemon;
+import com.pokemon.daje.persistance.marshaller.BaseMarshaller;
+import org.springframework.stereotype.Component;
+import java.util.stream.Collectors;
+
+@Component
+public class PokemonToExchangeMarshaller implements BaseMarshaller<Pokemon, PokemonExchangeDTO> {
+
+    @Override
+    public Pokemon fromDTO(PokemonExchangeDTO dto) {
+        Pokemon pokemon = null;
+        if(dto != null && dto.getMoves() != null){
+                pokemon = new Pokemon();
+                pokemon.setId(dto.getId());
+                pokemon.setName(dto.getName());
+                pokemon.setCurrentHP(dto.getCurrentHP());
+                pokemon.setMaxHP(dto.getMaxHP());
+                pokemon.setOriginalTrainer(dto.getOriginalTrainer());
+                pokemon.setMoves(dto.getMoves().stream().map(Move::new).collect(Collectors.toSet()));
+        }
+        return pokemon;
+    }
+
+    @Override
+    public PokemonExchangeDTO toDTO(Pokemon business) {
+        PokemonExchangeDTO pokemonDTO = null;
+        if(business != null && business.getMoves() != null){
+                pokemonDTO = new PokemonExchangeDTO();
+                pokemonDTO.setId(business.getId());
+                pokemonDTO.setName(business.getName());
+                pokemonDTO.setType(business.getId());
+                pokemonDTO.setMoves(business.getMoves().stream().map(Move::getId).collect(Collectors.toSet()));
+                pokemonDTO.setCurrentHP(business.getCurrentHP());
+                pokemonDTO.setMaxHP(business.getMaxHP());
+                pokemonDTO.setOriginalTrainer(business.getOriginalTrainer());
+        }
+        return pokemonDTO;
+    }
+}
