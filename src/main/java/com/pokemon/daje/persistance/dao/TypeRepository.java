f24a4180a525464fa5c8d786ac5b70666f6b6621
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-24 18:52:55 +0200

    feat ( add method to find type by pokedexId or else fallback to unknown specie )

--- src/main/java/com/pokemon/daje/persistance/dao/TypeRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/TypeRepository.java
@@ -1,10 +1,15 @@
 package com.pokemon.daje.persistance.dao;
 
+import com.pokemon.daje.persistance.dto.MoveDTO;
 import com.pokemon.daje.persistance.dto.TypeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TypeRepository extends JpaRepository<TypeDTO,Integer> {
     Optional<TypeDTO> findByPokedexId(int id);
+    @Query("select t from TypeDTO as t where t.pokedexId = :id or t.pokedexId = 30000 ORDER BY(t.pokedexId) limit 1")
+    Optional<MoveDTO> findByPokedexIdOrGetUnknow(@Param("id") int id);
 }
