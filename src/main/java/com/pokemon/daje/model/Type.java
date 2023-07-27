2e39bb0e63948b069b2dd339442617d14261e712
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-27 12:56:11 +0200

    fix ( add constructor with id for Type.java )

--- src/main/java/com/pokemon/daje/model/Type.java
+++ src/main/java/com/pokemon/daje/model/Type.java
@@ -1,16 +1,23 @@
 package com.pokemon.daje.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Objects;
 @Getter
 @Setter
+@NoArgsConstructor
 public class Type implements BusinessInterface{
 
     private Integer id;
     private String name;
     private String imageUrl;
+
+
+    public Type(Integer id) {
+        this.id = id;
+    }
 
     @Override
     public boolean equals(Object o) {
