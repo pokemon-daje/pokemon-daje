e59d642a22d31eb654e9b5022a6e017a1e659b9d
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-27 11:05:28 +0200

    refactor ( change management of card position )

--- daje-fe/template/main/script.js
+++ daje-fe/template/main/script.js
@@ -1,30 +1,44 @@
 let wrapper = null;
-let carousel = null;
 let arrowBtns = null;
 let carouselChildrens = null;
-let counter = -1;
+let counter = 0;
 let firstCardOffSetWidth = 0;
-let halfScreen= window.screen.width/2;
-let carouselWidth = 0;
+let halfScreen= 0;
 let cardsOffset = 0;
-let frame= 0;
 let curtainsAreOpen= false;
 
 function gatherData(){
-     wrapper = document.querySelector(".wrapper");
-     carousel = document.querySelector(".carousel-container");
-     arrowBtns = document.querySelectorAll(".wrapper i");
-     carouselChildrens = [...carousel.children];
-     counter = 0;
-     firstCardOffSetWidth = carouselChildrens[0].offsetWidth;
-    cardsOffset = carouselChildrens.length * firstCardOffSetWidth + firstCardOffSetWidth/2;
+    halfScreen= halfScreen = document.querySelector(".carousel-container").clientWidth/2;
+    wrapper = document.querySelector(".wrapper");
+    arrowBtns = document.querySelectorAll(".wrapper i");
+    carouselChildrens = [...document.querySelectorAll(".carousel-container li")];
+    counter = 0;
+    firstCardOffSetWidth = carouselChildrens[0].offsetWidth;
+}
+function gatherDataLoading(){
+    halfScreen = document.querySelector(".carousel-container").clientWidth/2;
+    wrapper = document.querySelector(".wrapper");
+    arrowBtns = document.querySelectorAll(".wrapper i");
+    carouselChildrens = [...document.querySelectorAll(".carousel-container li")];
+    counter = 0;
+    firstCardOffSetWidth = carouselChildrens[0].offsetWidth;
+    for(cardPoke of carouselChildrens){
+        let pokemonOfCard = modifiedPokemons.find(poke => poke.database_id == cardPoke.id);
+        console.log("card width",firstCardOffSetWidth)
+        let posPx = pokemonOfCard.pos*firstCardOffSetWidth + firstCardOffSetWidth/2 * pokemonOfCard.pos;
+
+        pokemonOfCard.originalPos = posPx;
+        cardPoke.style.transform = `translate(${posPx}px)`
+        cardPoke.style.zIndex = "1";
+    }
 }
 function nextImg(integer){
+    console.log("prova")
     if(wrapper === null){
         gatherData()
     }
     halfScreen= window.screen.width/2
-    carouselWidth = carousel.clientWidth
+    carouselChildrens = [...document.querySelectorAll(".carousel-container li")];
     firstCardOffSetWidth = carouselChildrens[0].offsetWidth;
     cardsOffset = carouselChildrens.length * firstCardOffSetWidth/2 + firstCardOffSetWidth/2;
     if(integer > 0){
@@ -33,10 +47,10 @@ function nextImg(integer){
     else{
         counter--;
     }
-    if(counter > carouselChildrens.length){
-        counter = 2;
-    }else if(counter < 2){
-        counter = carouselChildrens.length;
+    if(counter >= carouselChildrens.length){
+        counter = 0;
+    }else if(counter <= -1){
+        counter = carouselChildrens.length-1;
     }
     for(i=0;i<carouselChildrens.length;i++){
         animation(i);
@@ -44,7 +58,9 @@ function nextImg(integer){
 }
 
 function animation(rootCard){
-    carouselChildrens[rootCard].style.transform = `translate(${counter*(firstCardOffSetWidth)-firstCardOffSetWidth/2-cardsOffset}px)`
+    let htmlCardPokemon = carouselChildrens[rootCard];
+    let pokemonOfCard = modifiedPokemons.find(poke => poke.database_id == htmlCardPokemon.id);
+    carouselChildrens[rootCard].style.transform = `translate(${-(counter*firstCardOffSetWidth)+pokemonOfCard.originalPos+(firstCardOffSetWidth/2*pokemonOfCard.pos)-firstCardOffSetWidth*counter}px)`
     carouselChildrens[i].style.zIndex = "1";
 }
 
@@ -56,8 +72,8 @@ function openCurtains(){
         let buttonRight = document.querySelector("#right");
         let buttonDiv = document.querySelector(".buttons");
         buttonDiv.style.opacity = '1';
-        curtainLeft.style.transform = `translate(${-70}%)`;
-        curtainRight.style.transform = `translate(${+70}%)`;
+        curtainLeft.style.transform = `translate(${-60}%)`;
+        curtainRight.style.transform = `translate(${+60}%)`;
         buttonLeft.style.transform = 'rotate(360deg)';
         buttonRight.style.transform = 'rotate(-360deg)';
         getPokemons();
