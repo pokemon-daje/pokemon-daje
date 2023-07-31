d248f66f2b460e61107b93ffe43a25aff775ee3b
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-31 09:05:30 +0200

    feat ( add method to get move for pokemon )

--- src/main/java/com/pokemon/daje/persistance/dao/MoveRepository.java
+++ src/main/java/com/pokemon/daje/persistance/dao/MoveRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface MoveRepository extends JpaRepository<MoveDTO,Integer> {
     Optional<MoveDTO> findByPokedexId(int id);
     @Query("select m from MoveDTO as m where m.pokedexId = :id or m.pokedexId = 30000 ORDER BY(m.pokedexId) limit 1")
     Optional<MoveDTO> findByPokedexIdOrGetUnknow(@Param("id") int id);
+    @Query(value = "SELECT * from move as m where m.id = any(select ms.move_db_id from pokemons_move_set as ms where ms.pokemon_db_id = :pokemonDBId)",nativeQuery = true)
+    Set<MoveDTO> findAllByPokemonId(@Param("pokemonDBId") int pokemonDBId);
 }
