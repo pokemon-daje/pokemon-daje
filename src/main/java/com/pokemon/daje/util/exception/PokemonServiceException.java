991bb611a065449323f5b0064812ee2bd26933f4
Livarbesaru <liburdidaniele1998@gmail.com>
2023-07-31 14:14:55 +0200

    refactor ( add constructor )

--- src/main/java/com/pokemon/daje/util/exception/PokemonServiceException.java
+++ src/main/java/com/pokemon/daje/util/exception/PokemonServiceException.java
@@ -11,4 +11,7 @@ public class PokemonServiceException extends RuntimeException {
     public PokemonServiceException(String message, Throwable cause) {
         super(message, cause);
     }
+    public PokemonServiceException(String message) {
+        super(message);
+    }
 }
